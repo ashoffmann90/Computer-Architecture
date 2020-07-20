64f9631442300caf1ad16d2287bb0f769997548c
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-07-20 15:51:03 -0700

    sigh

---
+++ mondayproblem.py
@@ -0,0 +1,9 @@
+arr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+# loop through
+# for num in range(len(arr)-1, -1, -1):
+#     # print reverse
+#     print(arr[num])
+arr.reverse()
+for num in arr:
+    print(num)
