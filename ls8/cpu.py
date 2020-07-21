358e2aa11e60a3cce26e9e10b3ca73b3287bfe35
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-07-21 16:58:42 -0700

    dynamic

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,9 @@
 """CPU functionality."""
 
 import sys
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
 
 
 class CPU:
@@ -11,16 +14,15 @@ class CPU:
         self.pc = 0
         self.ram = [0] * 256
         self.reg = [0] * 8
-        # self.ir = {
-        #     0b10000010: 'LDI',
-        #     0b01000111: 'PRN',
-        #     0b00000001: 'HLT'
-        # }
-        # self.ir = {
-        #     'LDI': 0b10000010,
-        #     'PRN': 0b01000111,
-        #     'HLT': 0b00000001
-        # }
+        self.ir = {
+            'LDI': 0b10000010,
+            'PRN': 0b01000111,
+            'HLT': 0b00000001
+        }
+        self.branchtable = {}
+        self.branchtable[HLT] = self.hlt
+        self.branchtable[LDI] = self.ldi
+        self.branchtable[PRN] = self.prn
 
     def ram_read(self, MAR):
         return self.ram[MAR]
@@ -28,24 +30,74 @@ class CPU:
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
         address = 0
 
+        # open file
+        # print(sys.argv)
+        # with open(sys.argv[1]) as f:
+        #     for line in f:
+        #         try:
+        #             line2 = line.split('#')[0].strip()
+        #             if line2 == '':
+        #                 continue
+        #         except ValueError:
+        #             pass
+
+        #         line3 = int(line2, 2)
+        #         self.ram[address] = line3
+        #         address += 1
+
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        program = '''
+        # Print the number 8
 
-        for instruction in program:
+        # This comment and blank line is here to make sure
+        # they are handled correctly by the file reading        code.
+
+        10000010 # LDI R0,8
+        00000000
+        00001000
+        01000111 # PRN R0
+        00000000
+        00000001 # HLT
+        '''
+        # print(program)
+        # print(program.split('\n'))
+        split = program.split('\n')
+        cleaned = []
+        for line in filename:
+            line1 = line.strip()
+            if not line1.startswith('#') and line1.strip():
+                line2 = line1.split('#', 1)[0]
+                cleaned.append(int(line2, 2))
+                print(line2)
+
+        # for line in program:
+        #     try:
+        #         line2 = line.split('#')[0].strip()
+        #         if line2 == '':
+        #             continue
+        #     except ValueError:
+        #         pass
+        #     line3 = int(line2, 2)
+        #     self.ram[address] = line3
+        #     address += 1
+        # program = [
+        #     # # From print8.ls8
+        #     # 0b10000010,  # LDI R0,8
+        #     # 0b00000000,
+        #     # 0b00001000,
+        #     # 0b01000111,  # PRN R0
+        #     # 0b00000000,
+        #     # 0b00000001,  # HLT
+
+        # ]
+
+        for instruction in cleaned:
             self.ram[address] = instruction
             address += 1
 
@@ -80,37 +132,31 @@ class CPU:
 
     def ldi(self, register, value):
         self.reg[register] = value
+        self.pc += 3
 
-    def prn(self, index):
-        print(self.reg[index])
+    def prn(self, register, x):
+        print(self.reg[register])
+        self.pc += 2
 
-    def hlt(self):
+    def hlt(self, x, y):
         sys.exit(0)
+        self.pc += 1
 
     def run(self):
         """Run the CPU."""
         # number of operands = inst value & 0b11000000 >> 6
         # inst length = number of operands + 1
-        LDI = 0b10000010
-        PRN = 0b01000111
-        HLT = 0b00000001
+        # LDI = 0b10000010
+        # PRN = 0b01000111
+        # HLT = 0b00000001
+
         running = True
         # self.trace()
         while running is True:
             inst_reg = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc+1)
             operand_b = self.ram_read(self.pc+2)
-            if inst_reg == LDI:
-                # print('LDI')
-                # self.reg[operand_a] = operand_b
-                self.ldi(operand_a, operand_b)
-                self.pc += 3
-            elif inst_reg == PRN:
-                self.prn(operand_a)
-                self.pc += 2
-            elif inst_reg == HLT:
-                self.hlt()
-                self.pc += 1
-                running = False
+            if inst_reg in self.branchtable:
+                self.branchtable[inst_reg](operand_a, operand_b)
 
         # print(self.ir['LDI'])
